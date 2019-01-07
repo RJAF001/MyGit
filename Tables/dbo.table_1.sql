71423f22a82893c877af243e861ce7900e8d7140
rafinnerty@yahoo.co.uk <rafinnerty@yahoo.co.uk>
2019-01-07 16:49:56 +0000

    blah

---
+++ Tables/dbo.table_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[table_1] (
+  [x] [int] NOT NULL,
+  [y] [char](100) NULL,
+  [z] [nvarchar](50) NULL,
+  [a] [nchar](10) NULL,
+  CONSTRAINT [PK_table_1_x] PRIMARY KEY CLUSTERED ([x])
+)
+ON [PRIMARY]
+GO
